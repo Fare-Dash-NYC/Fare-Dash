c54dba933d3dd6352c4f20411ea09c68b990c1b4
Jan <moralesjan40@gmail.com>
2022-05-16 13:58:44 -0400

    app.js

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express')
 const faresRouter = reqiure('./routes/dogRouer')
 const app = express();
 const { pool } = require('./db.js');
+const userRouter = require('./routes/usersRouter')
 
 
 const PORT = process.env.port || 8080;
 app.use(express.json());
-
+app.use(cors())
+app.use(userRouter)
 
 //pool.query("INSERT INTO users (username) values($1)", [data.username])
 
-app.listen(PORT)
-
-// install dependencies in the new forked repo and copy/paste my current progress
+app.listen(PORT, ()=>{
+    console.log('app started ')
+})
